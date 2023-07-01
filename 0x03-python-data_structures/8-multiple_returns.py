ec30269b6f4b4f107c86bf94eda87f3ec97bbd06
Amr Elstohy <125589444+amrelstohy@users.noreply.github.com>
2023-07-01 20:51:13 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    if sentence == None:
+        return (0, None)
     if sentence:
         len_s = len(sentence)
         return((len_s, sentence[0]) if len_s != 0 else (None, sentence[0]))
