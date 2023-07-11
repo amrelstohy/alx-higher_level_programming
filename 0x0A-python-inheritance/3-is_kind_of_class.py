caa98d973ad35987c6796ad6da024418707e8413
Amr Elstohy <125589444+amrelstohy@users.noreply.github.com>
2023-07-11 03:19:04 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,4 @@ Contains the is same class function
 
 def is_kind_of_class(obj, a_class):
     """ returns a list of available and methodes of an objet"""
-    return(isinstance(obj, a_class))
+    return (isinstance(obj, a_class))
