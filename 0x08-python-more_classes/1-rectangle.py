fbeb5093db2c6e95b2c53b1cfd038e5b0d13b5ba
Amr Elstohy <125589444+amrelstohy@users.noreply.github.com>
2023-10-05 05:19:11 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""hi"""
+"""
+This module defines the a Rectangle Object.
+"""
 
 
-class rectangle:
-    """hi """
-
+class Rectangle:
+    """Retangle object with getter and setters
+    """
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -15,10 +17,10 @@ class rectangle:
 
     @width.setter
     def width(self, value):
-        if value != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -27,8 +29,8 @@ class rectangle:
 
     @height.setter
     def height(self, value):
-        if value != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
         self.__height = value
