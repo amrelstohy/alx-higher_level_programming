7f8352c82e731798f73883586fc6a2fa80ff51a7
Amr Elstohy <125589444+amrelstohy@users.noreply.github.com>
2023-10-05 17:02:56 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""Create a Rectangle class."""
+"""hi"""
 
 
-class Rectangle:
-    """Definition a class rectangle."""
-
+class rectangle:
+    """hi"""
+    
     def __init__(self, width=0, height=0):
-        """Initializing Rectangle.
-        """
-        self.width = width
         self.height = height
-
+        self.width = width
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
         return self.__width
-
     @width.setter
     def width(self, value):
-        if not type(int):
+        if value != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -26,23 +21,20 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
         return self.__height
-
+    
     @height.setter
     def height(self, value):
-        if not type(int):
+        if value != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """Calculate the area of the rectangle."""
-        return self.width * self.height
-
+        return self.__height * self.__width
+    
     def perimeter(self):
-        """Calculate the perimeter of the rectangle."""
-        if self.width == 0 or self.height == 0:
+        if self.__height or self.__width == 0:
             return 0
-        return (self.width + self.height) * 2
+        return (2 * self.__height) + (2 * self.__width)
