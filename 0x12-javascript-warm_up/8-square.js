79088fa80f848367bb229bde12704e9a09abbce3
Amr Elstohy <amramr2901@gmail.com>
2024-03-13 23:58:11 +0000

    hi

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+
+console.log = function () {
+  process.stdout.write(Array.prototype.join.call(arguments, '') + '');
+};
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      console.log('x');
+    }
+    console.log('\n');
+  }
+}
