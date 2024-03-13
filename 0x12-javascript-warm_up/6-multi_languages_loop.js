b503c1acf4e952b1b79de29a0d91fd02d17254a0
Amr Elstohy <amramr2901@gmail.com>
2024-03-13 23:29:33 +0000

    hi

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-let i = 0; 
+let i = 0;
 const mylist = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 while (mylist[i]) {
   console.log(mylist[i]);
   i = i + 1;
 }
-
