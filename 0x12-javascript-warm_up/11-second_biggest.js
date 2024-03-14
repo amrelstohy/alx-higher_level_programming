b558f2e9ed19a6363f6f15dde54850cb2e1d14de
Amr Elstohy <amramr2901@gmail.com>
2024-03-14 01:09:23 +0000

    hi

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
+let i = 2;
 const a = process.argv;
+const b = [];
 
 if (a.length === 2 || a.length === 3) {
   console.log(0);
 } else {
-  a.sort();
-  a.reverse();
-  console.log(a[1]);
+  while (a[i]) {
+    b[i - 2] = parseInt(a[i]);
+    i++;
+  }
+  b.sort(function (a, b) { return a - b; });
+  b.reverse();
+  console.log(b[1]);
 }
