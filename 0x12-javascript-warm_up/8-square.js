fdc197e485ad661693c49ccadac33f2c60459839
Amr Elstohy <amramr2901@gmail.com>
2024-03-14 00:01:06 +0000

    hi

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ console.log = function () {
 };
 
 if (isNaN(size)) {
-  console.log('Missing size');
+  console.log('Missing size\n');
 } else {
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
