aabe860da6c5701d0052b7fd3ae16eecc41daa76
amrelstohy <amramr2901@gmail.com>
2024-04-17 01:52:05 +0200

    hi

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+
+"""
+you turn the lights in then you get them back off
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+base = declarative_base()
+
+
+class State(base):
+    """state class"""
+    __tablename__ = 'states'
+    id = Column(
+        Integer,
+        autoincrement=True,
+        primary_key=True,
+        nullable=False
+        )
+    name = Column(
+        String(128),
+        nullable=False
+        )
