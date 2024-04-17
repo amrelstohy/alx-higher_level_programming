ab84f7cce99e2e1d57eee27c324b0df108370907
amrelstohy <amramr2901@gmail.com>
2024-04-17 03:24:43 +0200

    hi

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
         pool_pre_ping=True
         )
     session = Session(engine)
-    session.query(State).order_by('states.id').all()
+    session.query(State).order_by(states.id).all()
     
