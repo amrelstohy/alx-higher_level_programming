09b54eae2bf206631a79882192d622b3ce6f07e6
amrelstohy <amramr2901@gmail.com>
2024-04-18 22:09:36 +0200

    hi

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+ a script that prints all City objects from the database
+ """
+"""from model_state import Base, State """
+from model_city import City
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True
+        )
+    session = Session(engine)
+    data = session.query(City).order_by(City.id).all()
+    for i in data:
+        print("{}: ({}) <{}>".format(i.state_id, i.id, i.name))
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+ a file similar to model_state.py that contains the class definition of a City
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """city class"""
+
+    __tablename__ = "cities"
+    id = Column(
+        Integer,
+        autoincrement=True,
+        primary_key=True,
+        nullable=False
+        )
+    name = Column(
+        String(128),
+        nullable=False
+        )
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+        )
