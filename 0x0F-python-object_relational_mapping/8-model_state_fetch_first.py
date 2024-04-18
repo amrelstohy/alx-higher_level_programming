8e2440d261758caf1f07a1cf3a284a1eabe94c4b
amrelstohy <amramr2901@gmail.com>
2024-04-18 19:42:44 +0200

    hi

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-
 """
-queries and sqlalchemy
+python script that ---
 """
 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import Base, State
+import re
 import sys
+from model_state import Base, State
 
+from sqlalchemy import (create_engine)
+from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(bind=engine)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], str(sys.argv[2]), sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    row = session.query(State).order_by(State.id).first()
-    if row:
-        print("{}: {}".format(row.id, row.name))
-    else:
+    first = session.query(State).order_by(State.id).first()
+    if first is None:
         print("Nothing")
-    session.close()
+    else:
+        print("{}: {}".format(first.id, first.name))
