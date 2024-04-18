ea8249b2afbda58fa61bbe3906b96fb6a66eb2ab
amrelstohy <amramr2901@gmail.com>
2024-04-18 19:17:32 +0200

    hi

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,4 +16,7 @@ if __name__ == '__main__':
         pool_pre_ping=True)
     session = Session(engine)
     data = session.query(State).first()
-    print("{}: {}".format(data.id, data.name))
+    if (data == None):
+        print("Nothing")
+    else:
+        print("{}: {}".format(data.id, data.name))
