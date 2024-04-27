36f5c2d0b14364680d72c68bedf000c942e523c4
amrelstohy <amramr2901@gmail.com>
2024-04-28 02:51:17 +0300

    hi

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,9 +6,9 @@ Find a peak
 
 def find_peak(list_of_integers):
     """a function that finds a peak in a list of unsorted integers."""
-    
+
     x = list_of_integers
-    l,r = 0, len(x) - 1
+    l, r = 0, len(x) - 1
     if (len(x) == 0):
         return "None"
     while l < r:
