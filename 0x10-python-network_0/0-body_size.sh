098825ea7679bf0f85dd8489824eb9c0de5ea27e
Amr Elstohy <125589444+amrelstohy@users.noreply.github.com>
2024-04-27 23:45:04 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # displays the size of the body of the response
-<<<<<<< HEAD
 curl -s -w '%{size_download}' -o /dev/null "$1"
-=======
-curl -s -w '%{size_download}' -o /dev/null "&1"
->>>>>>> 100db587450c7c3edc4d480cbc0b0f17ea043ae8
+
